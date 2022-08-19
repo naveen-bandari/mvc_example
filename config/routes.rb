4c4ab84073b2b312ab5d3af1104de3ba2b0aea28
Naveen Bandari <naveen.bandari@prasthana.com>
2022-08-19 08:02:49 +0530

    added new routes for errors info

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :articles
   get '/ping', to: 'ping#info', as: 'ping'
+  get '/error_info', to: 'ping#error_info'
   # root 'articles#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
